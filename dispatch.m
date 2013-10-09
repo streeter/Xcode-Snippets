fde6119cf98f66caf1075d041e995ac245f8ea35
Chris Streeter <chris@chrisstreeter.com>
2013-10-09 16:09:12 -0700

    Add dispatch snippet

---
+++ dispatch.m
@@ -0,0 +1,10 @@
+// dispatch_async Pattern for Moving to the Foreground
+// Dispatch to do move back to the foreground with results.
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+dispatch_async(dispatch_get_main_queue(), ^{
+    <#code#>
+});
